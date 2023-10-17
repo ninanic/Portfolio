23c2f60f7768a934bc7f000fbdfe44a4ec8cf87e
Nina Knight <ninaknight45@gmail.com>
2023-10-16 22:42:42 -0700

    updated folder

---
+++ Exercises/Javascript/script.js
@@ -0,0 +1,16 @@
+var num1 = 10;
+
+var num2 = 20;
+
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotient = num1 / num2;
+
+console.log(sum);
+
+console.log(difference);
+
+console.log(product);
+
+console.log(quotient);
